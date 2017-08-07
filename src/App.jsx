b50d6b3af1714ecf4e4b5db7986c3f6882a5ef80
John-Philip Johansson <seriema@gmail.com>
2017-08-07 11:47:37 -0300

    Add page footer
    Also changed page header to match styling.

--- src/App.jsx
+++ src/App.jsx
@@ -12,35 +12,43 @@ import './App.css';
 
 const App = () => {
     return (
-        <div className="App container mt-4">
+        <div className="App">
             <div className="App-header">
                 <h2>Suginoharyu</h2>
             </div>
 
-            <div className="row">
-                <div className="col">
-                    <Settings>
-                        <p>Select the belt and techniques you want to study for.</p>
-                        <BeltDropdown />
-                        <CategoryCheckboxes />
-                        <HintCheckboxes />
-                    </Settings>
+            <div className="container mt-4">
+                <div className="row">
+                    <div className="col">
+                        <Settings>
+                            <p>Select the belt and techniques you want to study for.</p>
+                            <BeltDropdown />
+                            <CategoryCheckboxes />
+                            <HintCheckboxes />
+                        </Settings>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-lg-6">
+                        <PickCardButton />
+
+                        <TechniqueCard />
+                    </div>
+                    <div className="col-lg-6">
+                        <ToggleTechniquesTableButton />
+
+                        <TechniquesTable />
+                    </div>
                 </div>
             </div>
 
-            <div className="row">
-                <div className="col-lg-6">
-                    <PickCardButton />
-
-                    <TechniqueCard />
+            <footer className="App-footer text-muted">
+                <div className="container">
+                    <p>Hacked together with love by <a href="http://johansson.jp" target="_blank" rel="noopener noreferrer">John-Philip "JP" Johansson</a>. The source code is available on <a href="https://github.com/seriema/judo" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
+                    <p>These techniques follow the belt ranking of <a href="http://www.suginoharyu.com/">Suginoharyu</a>. These will most likely not match your dojo if you train somewhere else.</p>
                 </div>
-                <div className="col-lg-6">
-                    <ToggleTechniquesTableButton />
-
-                    <TechniquesTable />
-                </div>
-            </div>
-
+            </footer>
         </div>
     );
 };
